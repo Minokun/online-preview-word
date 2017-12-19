83734d767c9620b331c4b936bb9cee0c970d9206
wuxiaokun <952718180@qq.com>
2017-12-19 16:00:11 +0800

    文件路径配置修改

--- dataDeal/settings.py
+++ dataDeal/settings.py
@@ -1,6 +1,11 @@
+# 获取当前文件路径
+import os
+current_path = os.path.dirname(os.path.dirname(__file__))
 # 文件夹根目录
 ROOT_PATH = 'D:/workspace/testFile/'
 # 子文件夹
 DIR_LIST = ['出入登记', '财务公开', '政务公开', '业务公开']
 # 转换到的目标文件
-CONVERT_DIR_PATH = 'D:/workspace/testFile/html/'
+CONVERT_DIR_PATH = os.path.join(current_path, 'frontend/build/html/')
+if not os.path.exists(CONVERT_DIR_PATH):
+    os.mkdir(CONVERT_DIR_PATH)
